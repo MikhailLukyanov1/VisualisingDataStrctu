6c5ea12d477a9e0786373944fda7331d90cb520f
MikhailLukyanov1 <lukyanov.m@etoncollege.org.uk>
2023-03-14 17:52:40 +0000

    Initial Commit

---
+++ VisualisingDataStructures/VisualisingDataStructuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisualisingDataStructuresApp.swift
+//  VisualisingDataStructures
+//
+//  Created by MIKHAEL LUKYANOV on 14/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct VisualisingDataStructuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
